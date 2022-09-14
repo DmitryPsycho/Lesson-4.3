f8ccb0f869b38a53c9b0ee1d230f7c2ef0e310c6
DmitryPsycho <kovalenko2511@bk.ru>
2022-09-14 20:26:09 +0300

    Initial Commit

---
+++ Lesson 4.3/Lesson_4_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_3App.swift
+//  Lesson 4.3
+//
+//  Created by Psycho on 14.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
